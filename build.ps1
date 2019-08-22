886afb12e163dbd9b48de9934ab9e83ae4c28663
Alena Hrynkevich <alena@appveyor.com>
2019-08-22 13:40:19 -0700

    Create build.ps1

---
+++ build.ps1
@@ -0,0 +1,5 @@
+nuget restore Services\WordCount\WordCount.sln
+msbuild Services\WordCount\WordCount.sln
+
+nuget restore Actors\VoiceMailBox\VoiceMailBox.sln
+msbuild Actors\VoiceMailBox\VoiceMailBox.sln
